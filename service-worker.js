5aa0b139d8a5fabe2009b7709d6928ea032c2573
Broutard <broutard@gmail.com>
2020-12-01 12:13:26 +0000

    Deploying to gh-pages from  @ 84d1e25f38a5c9ae9a27c07a6a278bd68bcb9b9d 🚀

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/engapp/precache-manifest.37b66fae647f360167ccab073e1cb33a.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "owlways"});
+
+workbox.core.skipWaiting();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
