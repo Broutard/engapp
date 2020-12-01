cf63c2e44f02c8ab4e2c498b8ac83bec64895017
Broutard <broutard@gmail.com>
2020-12-01 14:49:28 +0000

    Deploying to gh-pages from  @ 39cee8fc560f6e8c052d3c6237fe1a9e8b39a56c 🚀

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+importScripts("/engapp/precache-manifest.d131ed82a5a1da38eeac07cebd01cad5.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+// This is the code piece that GenerateSW mode can't provide for us.
+// This code listens for the user's confirmation to update the app.
+self.addEventListener('message', (e) => {
+  if (!e.data) {
+    return;
+  }
+
+  switch (e.data) {
+    case 'skipWaiting':
+      self.skipWaiting();
+      break;
+    default:
+      // NOOP
+      break;
+  }
+});
+
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
